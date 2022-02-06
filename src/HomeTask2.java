8d97a1dec4d659bf6ea724667bc6a5030bb9143e
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2022-02-06 21:29:53 +0200

    Home task 2

--- src/HomeTask2.java
+++ src/HomeTask2.java
@@ -9,13 +9,13 @@ public class HomeTask2 {
         System.out.println("Введите ширину прямоугольника:");
         Scanner width = new Scanner(System.in);
         int b = width.nextInt(); //засетали ширину в переменную b
-        double S = a*b;
-        double P = 2*(a+b);
+        int S = a*b;
+        int P = 2*(a+b);
         Date date = new Date();
         System.out.printf("Сторона a= %1$d%n" +
                 "Сторона b= %2$d%n" +
-                "Площадь прямоугольника равна S= %3$.1f%n" +
-                "Периметр прямоугольника равен P= %4$.1f%n" +
+                "Площадь прямоугольника равна S= %3$d%n" +
+                "Периметр прямоугольника равен P= %4$d%n" +
                 "Расчет был выполнен %5$tY - %5$tB - %5$td - %5$tT", a, b, S, P, date);
     }
 }
