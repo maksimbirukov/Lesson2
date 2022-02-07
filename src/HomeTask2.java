3927591c7cf6531447124dca86672b13cb9538a2
maksimbirukov <maximbirukov@gmail.com>
2022-02-07 11:14:18 +0200

    refactoring

--- src/HomeTask2.java
+++ src/HomeTask2.java
@@ -10,11 +10,11 @@ public class HomeTask2 {
         int b = scan.nextInt(); //set width to var b
         int S = a*b;
         int P = 2*(a+b);
-        Date date = new Date();
+ 
         System.out.printf("Сторона a= %1$d%n" +
                 "Сторона b= %2$d%n" +
                 "Площадь прямоугольника равна S= %3$d%n" +
                 "Периметр прямоугольника равен P= %4$d%n" +
-                "Расчет был выполнен %5$tY - %5$tB - %5$td - %5$tT", a, b, S, P, date);
+                "Расчет был выполнен %5$tY - %5$tB - %5$td - %5$tT", a, b, S, P, new Date());
     }
 }
