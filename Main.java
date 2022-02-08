873f702aed0e900e5eca9c3eba3e0247d5f02aff
WARLOCK77 <99019837+WARLOCK77@users.noreply.github.com>
2022-02-08 14:40:56 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ua.levelup;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+System.out.print("Введите значние стороны a=");
+double a=scan.nextDouble();
+System.out.print("Введите значние стороны b=");
+double b=scan.nextDouble();
+double P=2*(a+b);
+double S=a*b;
+System.out.format("Периметр P="+P+" Площадь S="+S+"  Расчет был выполнен - %tY - %<tB - %<tA - %<tT", new Date());
+    }
+}
